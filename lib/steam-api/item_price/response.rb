72bf9b26a10b61f7ac5e1d43aef98c8d2629328c
George Pogrebnoi <gosha1416@yandex.ru>
2024-05-09 19:03:20 +0300

    Add exception handler to ItemPrice

--- lib/steam-api/item_price/response.rb
+++ lib/steam-api/item_price/response.rb
@@ -6,10 +6,14 @@ module SteamAPI
       def initialize(params)
         super(params)
 
+        begin
         if @response["success"]
           @lowest_price = @response['lowest_price']
           @median_price = @response['lowest_price']
           @volume = @response['volume']
+        end
+        rescue NoMethodError => e
+          puts "ERROR #{e.exception} WITH #{params}"
         end
       end
     end
